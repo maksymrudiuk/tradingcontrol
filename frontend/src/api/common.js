d84f75ac5bf55389e13febe67553dd460e8869e1
Maksym <maxrudiyk@gmail.com>
2019-01-25 21:56:34 +0200

    fix base url in common file axios

--- frontend/src/api/common.js
+++ frontend/src/api/common.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const HTTP = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-    ? 'https://trading-control.tk/'
+    ? 'https://trading-control.tk/api/v1/'
     : 'http://127.0.0.1:8000/api/v1/'
 })
